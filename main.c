cd2e9e04bb1d367d8ab9f2db1d2925bbbfb4281a
sangji <83937801+leesangji5@users.noreply.github.com>
2023-04-01 08:20:22 +0900

    Update main.c

--- main.c
+++ main.c
@@ -1,38 +1,50 @@
 #include <stdio.h>
 #include <stdbool.h>
 
-int main() {
-	printf("Student Record System");
-	printf("1. add stuendt");
-	printf("2. remove stuendt");
-	printf("3. search stuendt");
-	printf("4. add stuendt");
-	printf("5. Exit");
-	return 0;
-}
+struct students
+{
+	char name[32];
+	int age;
+};
 
-int save() {
+int save_student() {
 	printf("save");
 }
 
 int add_student() {
 	char name[32];
-	int age;
-	char s;
+	int age=0;
+	char col[16];
+	bool save=true;
+	printf("a");
 
-	bool run = true;
-	while (run) {
-		printf("add student");
-		if (scanf_s("name: %s", &name) == 0) {
-			printf("input is wrong");
-		}
-		else {
-			scanf_s("age: %d", &age);
-
-			scanf_s("press y or yes to save this %c", s);
-			
-			run = false;
+	while (save) {
+		printf("student name: ");
+		scanf_s("%c", &name);
+		printf("student age: ");
+		scanf_s("%d", &age);
+		printf("is this collect? (y, yes or n, no): ");
+		scanf_s("%c", col);
+		if (col == ("y" || "yes")) {
+			save = false;
 		}
 	}
+	struct students student = { name, age };
+	save_student();
+}
 
+int main() {
+	int sel = 0;
+	printf("Student Record System\n");
+	printf("1. add student\n");
+	printf("2. remove student\n");
+	printf("3. search student\n");
+	printf("4. edit student\n");
+	printf("5. Exit\n");
+	printf("select number: ");
+	scanf_s("%d", &sel);
+	if (sel == 1) {
+		add_student();
+	}
+	return 0;
 }
