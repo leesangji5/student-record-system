0bcf834342f623e2eae490cee92ed0820196444b
sangji <83937801+leesangji5@users.noreply.github.com>
2023-04-01 10:45:03 +0900

    Update main.c

--- main.c
+++ main.c
@@ -1,40 +1,54 @@
 #include <stdio.h>
 #include <stdbool.h>
+#include <string.h>
 
-struct students
-{
-	char name[32];
-	int age;
-};
+void save_student(char name[32], int age) {
+	char student_imf[64]="";
+	sprintf_s(student_imf, sizeof(student_imf), "%s %d \n", name, age);
+	printf("saved\n");
+	printf("name : %s\n", name);
+	printf("age : %d", age);
 
-int save_student() {
-	printf("save");
+	FILE* sn;
+	fopen_s(&sn, "student.txt", "a");
+	fputs(student_imf, sn);
+	fclose(sn);
 }
 
 int add_student() {
 	char name[32];
-	int age=0;
+	int age = 0;
 	char col[16];
-	bool save=true;
-	printf("a");
+	bool save_imf = false;
 
-	while (save) {
+	while (true) {
 		printf("student name: ");
-		scanf_s("%c", &name);
+		scanf_s("%s", &name, 32);
 		printf("student age: ");
 		scanf_s("%d", &age);
-		printf("is this collect? (y, yes or n, no): ");
-		scanf_s("%c", col);
-		if (col == ("y" || "yes")) {
-			save = false;
+		printf("is this collect? (y, yes or n, no or exit): ");
+		scanf_s("%s", &col, 16);
+		if (!(strcmp(col, "y")) || !(strcmp(col, "yes"))) {
+			save_imf = true;
+			break;
+		}
+		else if (!(strcmp(col, "exit"))) {
+			break;
 		}
 	}
-	struct students student = { name, age };
-	save_student();
+	if (save_imf) {
+		save_student(name, age);
+		return 1;
+	}
+	return 0;
+}
+
+int remove_student() {
+	return 0;
 }
 
 int main() {
-	int sel = 0;
+	int select = 0;
 	printf("Student Record System\n");
 	printf("1. add student\n");
 	printf("2. remove student\n");
@@ -42,9 +56,12 @@ int main() {
 	printf("4. edit student\n");
 	printf("5. Exit\n");
 	printf("select number: ");
-	scanf_s("%d", &sel);
-	if (sel == 1) {
+	scanf_s("%d", &select);
+	if (select == 1) {
 		add_student();
+	}
+	if (select == 2) {
+		remove_student();
 	}
 	return 0;
 }
