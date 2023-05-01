addcc69aa0486ff4f8656f2f7ceeb8f7b34091e7
sangji <83937801+leesangji5@users.noreply.github.com>
2023-05-01 11:44:33 +0900

    Update main.c

--- main.c
+++ main.c
@@ -5,19 +5,18 @@
 // 기록 가능한 학생 수
 #define buf_lenth 100
 // 저장 가능 정보 개수
-#define inf_num 6
-#define _CRT_SECURE_NO_WARNINGS
+#define inf_num 7
 
-// 학생 정보 구조체
-// 추가 예정: 학번, 전번, 주소
+// 학생 정보 구조체s
 struct student_information
 {
 	int number;
 	char name[64];
 	int age;
 	int grade;
-	char phone[16];
+	char phone[64];
 	char adress[64];
+	char memo[1024];
 };
 
 // 학생수
@@ -37,7 +36,7 @@ int getTotalLine() {
 // 리스트 마지막에 학생 추가
 void save_student(struct student_information stu_inf) {
 	char student_inf[1024] = "";
-	sprintf_s(student_inf, sizeof(student_inf), "%d %s %d %d %s %s\n", getTotalLine() + 1, stu_inf.name, stu_inf.age, stu_inf.grade, stu_inf.phone, stu_inf.adress);
+	sprintf_s(student_inf, sizeof(student_inf), "%d,%s,%d,%d,%s,%s, \n", getTotalLine() + 1, stu_inf.name, stu_inf.age, stu_inf.grade, stu_inf.phone, stu_inf.adress);
 	printf("saved\n");
 	printf("number : %d\n", getTotalLine() + 1);
 	printf("name : %s\n", stu_inf.name);
@@ -70,15 +69,15 @@ int add_student() {
 	while (true) {
 		printf("------------------\n");
 		printf("student name(no space): ");
-		scanf_s("%s", &stu_inf.name, 64);
+		scanf_s(" %[^\n]", &stu_inf.name, 64);
 		printf("student age: ");
 		scanf_s("%d", &stu_inf.age);
 		printf("student grade: ");
 		scanf_s("%d", &stu_inf.grade);
 		printf("student phone: ");
-		scanf_s("%s", &stu_inf.phone, 16);
+		scanf_s(" %[^\n]", &stu_inf.phone, 64);
 		printf("student adress: ");
-		scanf_s("%s", &stu_inf.adress, 64);
+		scanf_s(" %[^\n]", &stu_inf.adress, 64);
 		printf("is this collect? (y or n or exit): ");
 
 		fflush(stdin);
@@ -142,7 +141,6 @@ int search_student() {
 	return 0;
 }
 
-// 학생이 없으면 return none
 // num번째 학생 정보를 구조체로 반환
 struct student_information read_student_information(int num) {
 	struct student_information stu_inf;
@@ -150,68 +148,71 @@ struct student_information read_student_information(int num) {
 	char buf[1024];
 	fopen_s(&fp, "student.txt", "r");
 
-	// num번째 학생을 불러옴
 	for (int i = 0; i < num; i++)
 		fgets(buf, sizeof(buf), fp);
 
 	// 몇번째를 기준으로 나누는지
 	int i = 0;
 	int j = 0;
-	int dir[inf_num-1];
+	int dir[inf_num - 1];
 	while (buf[i]) {
-		if ((int)buf[i] == (int)*" ") {
+		if ((int)buf[i] == (int)*",") {
 			dir[j] = i;
 			j++;
 		}
 		i++;
 	}
-	// 임시 저장
+
 	char number_[64];
 	char age_[64];
-	char grade_[16];
-	char phone_[16];
+	char grade_[64];
+	char phone_[64];
 	char adress_[64];
+	char memo_[1024];
 
 	// 이 줄의 끝이 몇번째인지
 	int last_null = 0;
 	while (buf[last_null] != NULL)
 		last_null++;
 
-	//
-	int d = 0;
 
+	int d = 0;
 	for (int i = 0; i < dir[d]; i++)
 		number_[i] = buf[i];
 	number_[dir[0]] = NULL;
 	stu_inf.number = atoi(number_);
 	d++;
 
-	// 이름을 불러오고 마지막에 null 추가
 	for (int i = 0; i < dir[d] - dir[d - 1] - 1; i++)
 		stu_inf.name[i] = buf[i + dir[d - 1] + 1];
-	stu_inf.name[dir[d] - dir[d-1]-1] = NULL;
+	stu_inf.name[dir[d] - dir[d - 1] - 1] = NULL;
 	d++;
 
 	for (int i = 0; i < dir[d] - dir[d - 1] - 1; i++)
 		age_[i] = buf[i + dir[d - 1] + 1];
-	age_[dir[d] - dir[d - 1]-1] = NULL;
+	age_[dir[d] - dir[d - 1] - 1] = NULL;
 	stu_inf.age = atoi(age_);
 	d++;
 
 	for (int i = 0; i < dir[d] - dir[d - 1] - 1; i++)
 		grade_[i] = buf[i + dir[d - 1] + 1];
-	grade_[dir[d] - dir[d - 1]-1] = NULL;
+	grade_[dir[d] - dir[d - 1] - 1] = NULL;
 	stu_inf.grade = atoi(grade_);
 	d++;
 
 	for (int i = 0; i < dir[d] - dir[d - 1] - 1; i++)
 		stu_inf.phone[i] = buf[i + dir[d - 1] + 1];
-	stu_inf.phone[dir[d] - dir[d - 1]-1] = NULL;
+	stu_inf.phone[dir[d] - dir[d - 1] - 1] = NULL;
 	d++;
 
 	for (int i = 0; i < last_null - dir[d - 1] - 2; i++)
 		stu_inf.adress[i] = buf[i + dir[d - 1] + 1];
-	stu_inf.adress[last_null - dir[d - 1]-2] = NULL;
+	stu_inf.adress[dir[d] - dir[d - 1] - 1] = NULL;
+	d++;
+
+	for (int i = 0; i < last_null - dir[d - 1] - 2; i++)
+		stu_inf.memo[i] = buf[i + dir[d - 1] + 1];
+	stu_inf.memo[last_null - dir[d - 1] - 2] = NULL;
 	d++;
 
 	fclose(fp);
@@ -220,18 +221,18 @@ struct student_information read_student_information(int num) {
 
 // 학생 삭제시 번호 재배열
 int rearrangement() {
-	FILE* fp;
 	struct student_information stu_inf;
 	int line = getTotalLine();
 	char buf[buf_lenth][1024];
 
 	for (int i = 0; i < line; i++) {
-		stu_inf = read_student_information(i+1);
-		sprintf_s(buf[i], sizeof(buf[i]), "%d %s %d %d %s %s\n", i+1, stu_inf.name, stu_inf.age, stu_inf.grade, stu_inf.phone, stu_inf.adress);
+		stu_inf = read_student_information(i + 1);
+		sprintf_s(buf[i], sizeof(buf[i]), "%d,%s,%d,%d,%s,%s,%s\n", i + 1, stu_inf.name, stu_inf.age, stu_inf.grade, stu_inf.phone, stu_inf.adress, stu_inf.memo);
 	}
 	resave(buf);
 }
 
+// 번호로 학생 정보 찾기
 int find_student_use_number() {
 	int num;
 
@@ -243,11 +244,15 @@ int find_student_use_number() {
 		printf("please enter again\n");
 	}
 	struct student_information stu_inf = read_student_information(num);
-	printf("%d %s %d %d %s %s\n", stu_inf.number, stu_inf.name, stu_inf.age, stu_inf.grade, stu_inf.phone, stu_inf.adress);
+	printf("%d, %s, %d, %d, %s, %s\n\n", stu_inf.number, stu_inf.name, stu_inf.age, stu_inf.grade, stu_inf.phone, stu_inf.adress);
 	return 0;
 }
 
-// 번호로 학생 정보 찾기
+int find_studnet_use_name() {
+	char name;
+}
+
+// 학생 검색
 int find_student() {
 	char how[16] = "";
 	printf("------------------\n");
@@ -259,15 +264,12 @@ int find_student() {
 
 		if (!strcmp(how, "num") || !strcmp(how, "number")) {
 			find_student_use_number();
-			break;
 		}
 		else if (!strcmp(how, "exit"))
 			break;
 		else
 			printf("please enter again\n");
 	}
-
-	find_student_use_number();
 
 	printf("------------------\n\n");
 	return 0;
@@ -287,11 +289,12 @@ int edit_student() {
 	struct student_information stu_inf = read_student_information(number);
 	FILE* fp;
 	char buf[buf_lenth][1024];
-	char what[64]="";
-	char name[64]="";
-	char phone[16] = "";
+	char what[64] = "";
+	char name[64] = "";
+	char phone[64] = "";
 	char adress[64] = "";
-	char _[1024]="";
+	char memo[1024] = "";
+	char _[1024] = "";
 	int num = 0;
 	int age = 0;
 	int grade = 0;
@@ -309,49 +312,49 @@ int edit_student() {
 	printf("grade: %d\n", stu_inf.grade);
 	printf("phone: %s\n", stu_inf.phone);
 	printf("adress: %s\n", stu_inf.adress);
+	printf("memo: %s\n", stu_inf.memo);
 
 	while (true) {
-		printf("\nedit inf: ");
+		printf("\nedit inf (exit): ");
 		scanf_s("%s", &what, 64);
+		fflush(stdin);
 
-		if (!(strcmp(what, "all"))) {
-			continue;
-		}
-		else if (!(strcmp(what, "name"))) {
+		if (!(strcmp(what, "name"))) {
 			printf("name: ");
-			scanf_s("%s", &name, 64);
+			scanf_s(" %[^\n]", &name, 64);
 			strcpy_s(stu_inf.name, 64, name);
-			break;
 		}
 		else if (!(strcmp(what, "age"))) {
 			printf("age: ");
 			scanf_s("%d", &age);
 			stu_inf.age = age;
-			break;
 		}
 		else if (!(strcmp(what, "grade"))) {
 			printf("grade: ");
 			scanf_s("%d", &grade);
 			stu_inf.grade = grade;
-			break;
 		}
 		else if (!(strcmp(what, "phone"))) {
 			printf("phone: ");
-			scanf_s("%s", &phone, 16);
-			strcpy_s(stu_inf.phone, 16, phone);
-			break;
+			scanf_s("%s", &phone, 64);
+			strcpy_s(stu_inf.phone, 64, phone);
 		}
 		else if (!(strcmp(what, "adress"))) {
 			printf("adress: ");
 			scanf_s("%s", &adress, 64);
 			strcpy_s(stu_inf.adress, 64, adress);
+		}
+		else if (!(strcmp(what, "memo"))) {
+			printf("memo: ");
+			scanf_s(" %[^\n]", &memo, 64);
+			strcpy_s(stu_inf.memo, 1024, memo);
+		}
+		else if (!(strcmp(what, "exit")))
 			break;
-		}
-		else {
+		else
 			printf("please enter again");
-		}
 	}
-	sprintf_s(buf[stu_inf.number - 1], sizeof(buf[stu_inf.number]), "%d %s %d %d %s %s\n", stu_inf.number, stu_inf.name, stu_inf.age, stu_inf.grade, stu_inf.phone, stu_inf.adress);
+	sprintf_s(buf[stu_inf.number - 1], sizeof(buf[stu_inf.number]), "%d,%s,%d,%d,%s,%s,%s\n", stu_inf.number, stu_inf.name, stu_inf.age, stu_inf.grade, stu_inf.phone, stu_inf.adress, stu_inf.memo);
 	resave(buf, stu_inf);
 	printf("saved\n");
 	printf("------------------\n\n");
@@ -362,7 +365,7 @@ int edit_student() {
 int remove_student() {
 	FILE* fp;
 	char buf[buf_lenth][1024];
-	int num=1;
+	int num = 1;
 
 	fopen_s(&fp, "student.txt", "r");
 	for (int i = 0; i < getTotalLine(); i++)
@@ -373,7 +376,7 @@ int remove_student() {
 	while (true) {
 		printf("\nremove student number: ");
 		scanf_s("%d", &num);
-		if (0 < num && num < getTotalLine()+1)
+		if (0 < num && num < getTotalLine() + 1)
 			break;
 		printf("please enter again\n");
 	}
@@ -388,10 +391,11 @@ int remove_student() {
 		printf("grade: %d\n", stu_inf.grade);
 		printf("phone: %s\n", stu_inf.phone);
 		printf("adress: %s\n", stu_inf.adress);
+		printf("memo: %s\n", stu_inf.memo);
 		printf("remove this student (y or n):");
 		scanf_s("%s", &answer, 64);
 
-		if (!strcmp(answer, "n") || !strcmp(answer, "no") || 
+		if (!strcmp(answer, "n") || !strcmp(answer, "no") ||
 			!strcmp(answer, "y") || !strcmp(answer, "yes"))
 			break;
 		printf("please enter again\n");
@@ -402,6 +406,7 @@ int remove_student() {
 		sprintf_s(buf[getTotalLine() - 1], 1024, "\0");
 
 		resave(buf);
+		rearrangement();
 		printf("------------------\n\n");
 		return 1;
 	}
@@ -421,21 +426,30 @@ int file_check() {
 int main() {
 	file_check();
 	rearrangement();
+	int num_of_student = getTotalLine();
+	int select = 0;
+
 	while (true) {
-		int select = 0;
+		select = 0;
 		printf("------------------\n");
 		printf("Student Record System\n");
 		printf("1. add student\n");
 		printf("2. remove student\n");
 		printf("3. search student\n");
 		printf("4. find student\n");
-		printf("5. edit student\n");
+		printf("5. edit student(memo)\n");
 		printf("6. Exit\n");
+		if (num_of_student == 0)
+			printf("you should add student\n");
+		fflush(stdin);
 		printf("select number: ");
 		scanf_s("%d", &select);
 		printf("------------------\n\n");
+
 		if (select == 1)
 			add_student();
+		else if (num_of_student == 0 && select != 1)
+			printf("please enter again\nyou should add student\n\n");
 		else if (select == 2)
 			remove_student();
 		else if (select == 3)
@@ -446,7 +460,7 @@ int main() {
 			edit_student();
 		else if (select == 6)
 			break;
-		else 
+		else
 			printf("please enter again");
 	}
 	return 0;
